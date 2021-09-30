100136462edddd2ad56def446e709204dc6ee37d
cerchiariluiza <docente.luiza@gail.com>
2021-09-30 00:34:53 -0300

    f

---
+++ src/main/java/io/github/cursodsousa/todo/model/Todo.java
@@ -0,0 +1,84 @@
+package io.github.cursodsousa.todo.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Todo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String description;
+	
+	@Column
+	private Boolean done;
+	
+	@Column
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+	private LocalDateTime createdDate;
+	
+	@Column
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+	private LocalDateTime doneDate;
+
+	@PrePersist
+	public void beforeSave() {
+		setCreatedDate(LocalDateTime.now());
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Boolean getDone() {
+		return done;
+	}
+
+	public void setDone(Boolean done) {
+		this.done = done;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getDoneDate() {
+		return doneDate;
+	}
+
+	public void setDoneDate(LocalDateTime doneDate) {
+		this.doneDate = doneDate;
+	}
+	
+	
+}
